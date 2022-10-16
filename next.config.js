80e109d96f7972657b9fa9d2db6ac7cc27ae681f
TohidTaghavi <tohid.taghavi2000@yahoo.com>
2022-10-16 10:50:08 +0330

    feat: add eslint, commitlint, prettier, husky, docker, next-svg in their configs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withReactSvg = require('next-react-svg')
+const path = require('path')
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+module.exports = withReactSvg({
+  include: path.resolve(__dirname, 'assets/svg'),
+  webpack(config, options) {
+    return config
+  },
+  env: {
+    API_BASE_URL: 'www.google.com',
+  },
+})
